f7854b178e5cabb5a960caafa77fe5583885b82b
Vinícius Alves Pires <viniccius.alves.pires@gmail.com>
2025-04-01 15:55:50 -0300

    Delete: retirada de arquivos de teste helloWorld

--- src/main/java/com/vns/bank_api/repository/HelloInterface.java
+++
@@ -1,9 +0,0 @@
-package com.vns.bank_api.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.vns.bank_api.entity.HelloEntity;
-
-
-public interface HelloInterface extends JpaRepository<HelloEntity, Integer> {
-
-}
