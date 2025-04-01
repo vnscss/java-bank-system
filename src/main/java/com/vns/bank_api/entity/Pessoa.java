8d811d4e2998cd443e16c7de56ed3a0dfade39ee
Vinícius Alves Pires <viniccius.alves.pires@gmail.com>
2025-04-01 15:57:52 -0300

    Feat: Pessoa Class para ser herdada pelas Entitys Cliente e Gerente

---
+++ src/main/java/com/vns/bank_api/entity/Pessoa.java
@@ -0,0 +1,57 @@
+package com.vns.bank_api.entity;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public abstract class Pessoa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String cpf;
+    private String nome;
+    private Integer dataNasc;
+    private String role;
+
+    public Integer getId() {
+        return id;
+    }   
+
+
+    public String getCpf() {
+        return cpf;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+
+    
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+    public Integer getDataNasc() {
+        return dataNasc;
+    }
+    public void setDataNasc(Integer dataNasc) {
+        this.dataNasc = dataNasc;
+    }
+
+
+    public String getRole() {
+        return role;
+    }
+    public void setRole(Integer gerenteId ,String role) {
+        this.role = role;
+    }
+
+}
