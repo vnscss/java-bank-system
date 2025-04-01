9fb12d870a762f6edbd72f8232dc6c8d8c56f7bd
Vinícius Alves Pires <viniccius.alves.pires@gmail.com>
2025-04-01 15:56:20 -0300

    Feat: User entity e Repo

---
+++ src/main/java/com/vns/bank_api/entity/Usuarios.java
@@ -0,0 +1,40 @@
+package com.vns.bank_api.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuarios {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String username;
+    private String password;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
