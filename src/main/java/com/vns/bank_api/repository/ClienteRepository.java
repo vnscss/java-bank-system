aae7edd94670a8ef819ebddd74243f0a48417ef7
Vinícius Alves Pires <viniccius.alves.pires@gmail.com>
2025-04-02 19:38:14 -0300

    Feat: add findByUserId e Id

--- src/main/java/com/vns/bank_api/repository/ClienteRepository.java
+++ src/main/java/com/vns/bank_api/repository/ClienteRepository.java
@@ -1,6 +1,7 @@
 package com.vns.bank_api.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,4 +9,8 @@ import com.vns.bank_api.entity.Cliente;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
     List<Cliente> findByCpf(String cpf);
+
+    Optional<Cliente> findByUserId(Integer userId);
+
+    Optional<Cliente> findById(Integer id);
 }
