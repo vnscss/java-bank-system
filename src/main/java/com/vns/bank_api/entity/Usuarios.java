02e02a02b45165af37c26e4da63d61cc97e12e58
Vinícius Alves Pires <viniccius.alves.pires@gmail.com>
2025-04-03 12:52:38 -0300

    Fix: para citar o user na classe admins precisei fazer essa alteração

--- src/main/java/com/vns/bank_api/entity/Usuarios.java
+++ src/main/java/com/vns/bank_api/entity/Usuarios.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -17,6 +19,14 @@ public class Usuarios {
 
     private String username;
     private String password;
+
+    @ManyToOne
+    @JoinColumn(name = "admin_id") // Optional: specify the foreign key column name
+    private Admins admin;
+
+    public Admins getAdmins() {
+        return admin;
+    }
 
     public Integer getId() {
         return id;
